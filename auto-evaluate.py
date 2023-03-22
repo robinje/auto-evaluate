4ecd4ecce2ae881365ba420f8a0fc3c2004c1c1f
Jason E. Robinson <jason.robinson@auspex-labs.com>
2023-03-21 22:03:02 -0400

    WIP

--- auto-evaluate.py
+++ auto-evaluate.py
@@ -38,7 +38,7 @@ def analyze_code(code):
 
     def num_tokens_from_string(string: str) -> int:
         """Returns the number of tokens in a text string."""
-        encoding = tiktoken.get_encoding("p50k_base")
+        encoding = tiktoken.get_encoding("cl100k_base")
         num_tokens = len(encoding.encode(string))
         return num_tokens
 
